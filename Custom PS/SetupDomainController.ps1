725e8e08197da91d93cf1250564329d982c7e3bf
Khakzoy <reeftrik@gmail.com>
2022-06-16 10:55:56 +0200

    Additions

--- Custom PS/SetupDomainController.ps1
+++ Custom PS/SetupDomainController.ps1
@@ -1,6 +1,13 @@
-# Installeer domain services
+# Hernoem de Host
+Rename-Computer -NewName WIN2019DC
+
+# Installeer domain services & Management tools
 Add-WindowsFeature AD-Domain-Services
+Add-WindowsFeature RSAT-Role-Tools
 
 # Upgrade naar domain controller en domain settings
-
 Install-ADDSForest -DomainName test.local -InstallDNS
+
+# Root CA Install
+Install-WindowsFeature ADCS-Cert-Authority -IncludeManagementTools
+Install-AdcsCertificationAuthority -CAType StandaloneRootCA -CACommonName "WIN2019DC-CA" -CADistinguishedNameSuffix "DC=TEST,DC=LOCAL"-CryptoProviderName "RSA#Microsoft Software Key Storage Provider" -KeyLength 2048 -HashAlgorithmName SHA1 -ValidityPeriod Years -ValidityPeriodUnits 2 -DatabaseDirectory "C:\Windows\system32\certlog" -LogDirectory "C:\Windows\system32\CertLog" -Force 
